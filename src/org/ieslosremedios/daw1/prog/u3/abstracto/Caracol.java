f5266d0873e8bdfc5d12c6134afaf84f374e4293
Jonathan Leon Canto <jleocan773@g.educaand.es>
2022-12-12 13:30:53 +0100

    Update

--- src/org/ieslosremedios/daw1/prog/u3/abstracto/Caracol.java
+++ src/org/ieslosremedios/daw1/prog/u3/abstracto/Caracol.java
@@ -3,7 +3,6 @@ package org.ieslosremedios.daw1.prog.u3.abstracto;
 public class Caracol extends Animal {
     final static Integer numPatas = 0;
     final static Boolean mamifero = false;
-    @Override
     Integer getNumPatas() {
         return numPatas;
     }
--- src/org/ieslosremedios/daw1/prog/u3/abstracto/Perro.java
+++ src/org/ieslosremedios/daw1/prog/u3/abstracto/Perro.java
@@ -3,7 +3,6 @@ package org.ieslosremedios.daw1.prog.u3.abstracto;
 public class Perro extends Animal {
     final static Integer numPatas = 4;
     final static Boolean mamifero = true;
-    @Override
     Integer getNumPatas() {
         return numPatas;
     }
