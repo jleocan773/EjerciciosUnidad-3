4adaa671fd429f3c508c71771d1be3f7db4e74f9
jleocan773 <115530217+jleocan773@users.noreply.github.com>
2022-12-12 09:21:50 +0100

    Add files via upload

---
+++ Herencia/src/herencia/Hija.java
@@ -0,0 +1,23 @@
+package org.ieslosremedios.daw1.prog.u3.herencia;
+
+public class Hija extends Madre {
+
+    public Hija(){}
+
+    public void Presentar(String animalfavorito) { //Sobrecarga 1 porque existe un método con el mismo nombre pero aquí añadimos parámetros cuando el original no los tiene
+        System.out.println("Hola me llamo" + nombre + "y mi animal favorito es el " + animalfavorito);
+    }
+
+    public void Presentar(String animalfavorito, Integer edad){ //Sobrecarga 2 porque existe un método con el mismo nombre pero aquí añadimos parámetros cuando el original no los tiene
+        System.out.println("Hola me llamo " + nombre + ", mi animal favorito es el " + animalfavorito + " y tengo " + edad + " años.");
+    }
+
+    public void Presentar(){ //Sobrescritura 1
+        System.out.println("Soy " + getNombre());
+    } //Sobrescritura 1 porque existe un método con el mismo nombre y sin parámetros
+
+    public void AnimalFavorito(){ //Sobrescritura 2 porque existe un método con el mismo nombre y sin parámetros
+        System.out.println("El animal que más me gusta es el " + getAnimalfavorito());
+    }
+
+}
